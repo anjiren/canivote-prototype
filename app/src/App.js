109cedd21606d6775bc6e8cbda402f4f3d5a1860
Anji Ren <renalele@gmail.com>
2019-11-12 10:53:21 -0800

    Add more questions and styling

--- app/src/App.js
+++ app/src/App.js
@@ -6,57 +6,62 @@ import _ from 'lodash';
 const METER_MAX = 100;
 const METER_MIN = 0; // you lose when any meter reaches 0
 
+const DECISIONS_SHUFFLED = _.shuffle(DECISIONS); // shuffle once for a fresh game each time
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       currentDecisionIndex: 0,
-      currentDecision: DECISIONS[0],
+      currentDecision: DECISIONS_SHUFFLED[0],
       meters: {
         money: 50,
         timeWithFamily: 50,
         caringAboutVoting: 50
       },
-      gameOver: false
+      gameOver: false,
+      gameOverMeterNames: []
     }
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p className="civ-background">
-            Your name is Francis.
-            You are a 35 year old single mother of an 11 year old girl with a chronic illness.
-            You live in Florida and just became a citizen 6 months ago and are excited to participate in the presidential election.
-            You speak and read beginner English.
-          </p>
-          <div className="civ-meters">
-            <div>Money: {this.state.meters.money}</div>
-            <div>Time with family: {this.state.meters.timeWithFamily}</div>
-            <div>Caring about voting: {this.state.meters.caringAboutVoting}</div>
-          </div>
-          {
-            (this.state.gameOver || this.state.currentDecision) ?
-              <div className="civ-decision">
-                <p>{this.state.currentDecision.question}</p>
-                <button onClick={(e) => this.choose(this.state.currentDecision.choices[0])}>{this.state.currentDecision.choices[0].text}</button>
-                <button onClick={(e) => this.choose(this.state.currentDecision.choices[1])}>{this.state.currentDecision.choices[1].text}</button>
-              </div> : <p>Game over</p>
-          }
-        </header>
+        <p className="civ-background">
+          Your name is Francis.
+          You are a 35 year old single mother of an 11 year old girl with a chronic illness.
+          You live in Florida and just became a citizen 6 months ago and are excited to participate in the presidential election.
+          You speak and read beginner English.
+        </p>
+        <div className="civ-meters">
+          Your meters (don't let them fall to 0!)
+          <div>Money: {this.state.meters.money}</div>
+          <div>Time with family: {this.state.meters.timeWithFamily}</div>
+          <div>Caring about voting: {this.state.meters.caringAboutVoting}</div>
+        </div>
+        {
+          (!this.state.gameOver && this.state.currentDecision) ?
+            <div className="civ-decision">
+              <p>{this.state.currentDecision.question}</p>
+              <button className="civ-button" onClick={(e) => this.choose(this.state.currentDecision.choices[0])}>{this.state.currentDecision.choices[0].text}</button>
+              <button className="civ-button" onClick={(e) => this.choose(this.state.currentDecision.choices[1])}>{this.state.currentDecision.choices[1].text}</button>
+            </div> : <p>{this.state.gameOver ?
+              "Game over: Your " + this.state.gameOverMeterNames.join(" ") + " meter(s) fell to 0. You were not able to make it to the voting polls. Refresh to start over." :
+              "Congrats! You made it to the voting polls! Refresh to play again."}</p>
+        }
       </div>
     );
   }
 
   choose(choice) {
     let gameOver = false;
+    const gameOverMeterNames = [];
     const { meterDeltas } = choice;
     const newMeters = _.mapValues(this.state.meters, (meterValue, meterName) => {
       if (meterDeltas[meterName]) {
         const newMeterValue = meterValue + meterDeltas[meterName];
         if (newMeterValue <= METER_MIN) {
           gameOver = true;
+          gameOverMeterNames.push(meterName);
           return METER_MIN;
         } else if (newMeterValue >= METER_MAX) {
           return METER_MAX;
@@ -69,9 +74,10 @@ class App extends React.Component {
     });
     this.setState({
       currentDecisionIndex: this.state.currentDecisionIndex + 1,
-      currentDecision: DECISIONS[this.state.currentDecisionIndex + 1],
+      currentDecision: DECISIONS_SHUFFLED[this.state.currentDecisionIndex + 1],
       meters: newMeters,
-      gameOver: gameOver
+      gameOver: gameOver,
+      gameOverMeterNames: gameOverMeterNames
     });
   }
 }
--- app/src/decisions.js
+++ app/src/decisions.js
@@ -1,3 +1,28 @@
+/**
+ * TEMPLATE
+  {
+    question: "QUESTION TEXT HERE",
+    choices: [
+      {
+        text: "CHOICE 1 TEXT HERE",
+        meterDeltas: { // HOW CHOOSING CHOICE 1 AFFECTS METERS HERE
+          money: 0,
+          timeWithFamily: 0,
+          caringAboutVoting: 0
+        }
+      },
+      {
+        text: "CHOICE 2 TEXT HERE", 
+        meterDeltas: { // HOW CHOOSING CHOICE 2 AFFECTS METERS HERE
+          money: 0,
+          timeWithFamily: 0,
+          caringAboutVoting: 0
+        }
+      }
+    ],
+  },
+ */
+
 const DECISIONS = [
   // Decision 1
   {
@@ -6,15 +31,15 @@ const DECISIONS = [
       {
         text: "Post office job that pays $13 per hour with short and flexible shifts",
         meterDeltas: {
-          money: 5,
-          timeWithFamily: -5
+          money: 15,
+          timeWithFamily: -10
         }
       },
       {
         text: "Industrial kitchen cleaning job that pays $17 per hour with long and inflexible shifts",
         meterDeltas: {
-          money: 10,
-          timeWithFamily: -10
+          money: 30,
+          timeWithFamily: -30
         }
       }
     ],
@@ -26,15 +51,15 @@ const DECISIONS = [
       {
         text: "Used car",
         meterDeltas: {
-          money: -10,
-          timeWithFamily: 5,
+          money: -20,
+          timeWithFamily: 20,
         }
       },
       {
         text: "Daycare",
         meterDeltas: {
-          money: -5,
-          timeWithFamily: -5
+          money: -10,
+          timeWithFamily: -10
         }
       }
     ],
@@ -46,7 +71,7 @@ const DECISIONS = [
       {
         text: "Yes",
         meterDeltas: {
-          timeWithFamily: -5,
+          timeWithFamily: -20,
           caringAboutVoting: 10,
         }
       },
@@ -54,6 +79,84 @@ const DECISIONS = [
         text: "No",
         meterDeltas: {
           caringAboutVoting: -10
+        }
+      }
+    ],
+  },
+  // Decision 4
+  {
+    question: "A volunteer from the voting center calls your house offering help with getting registered to vote this year. Do you talk to him on the phone?",
+    choices: [
+      {
+        text: "Yes",
+        meterDeltas: {
+          caringAboutVoting: 10,
+        }
+      },
+      {
+        text: "No",
+        meterDeltas: {
+          caringAboutVoting: -10
+        }
+      }
+    ],
+  },
+  // Decision 5
+  {
+    question: "You can no longer afford the rent where you currently live. What do you do?",
+    choices: [
+      {
+        text: "Get another part-time job",
+        meterDeltas: {
+          money: 20,
+          timeWithFamily: -30,
+          caringAboutVoting: 10
+        }
+      },
+      {
+        text: "Move 45 minutes away to a different county",
+        meterDeltas: {
+          timeWithFamily: -10,
+          caringAboutVoting: -20
+        }
+      }
+    ],
+  },
+  // Decision 6
+  {
+    question: "Your daughter is sick and it seems worse than a cold. You don't know what she has. What do you do?",
+    choices: [
+      {
+        text: "Bring her to the hospital",
+        meterDeltas: {
+          money: -30,
+        }
+      },
+      {
+        text: "Treat her using home remedies",
+        meterDeltas: {
+          money: -20,
+          timeWithFamily: 10
+        }
+      }
+    ],
+  },
+  // Decision 7
+  {
+    question: "There are only a few days left to register to vote. Do you take half a day off work to go to the DMV?",
+    choices: [
+      {
+        text: "Yes",
+        meterDeltas: {
+          money: -20,
+          caringAboutVoting: 20
+        }
+      },
+      {
+        text: "No",
+        meterDeltas: {
+          money: 20,
+          caringAboutVoting: -20
         }
       }
     ],
